5dee90df1cbec8183deec95fc698a488463b9c65
Will <willian@crowdcast.jp>
2020-09-14 23:21:03 +0900

    addes signout component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { withAuthenticator } from "aws-amplify-react";
+import { AmplifySignOut } from "@aws-amplify/ui-react";
 
 import Todo from "./components/Todo";
+
+import "./components/todo.scss";
 
 import "@aws-amplify/ui/dist/style.css";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <div className="signout-container">
+        <AmplifySignOut />
+      </div>
       <Todo />
     </div>
   );
